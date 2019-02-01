0f2c2cea890f0a5117ac0927b3fce05dacf681ff
Alex <alex.bondarevskiy@gmail.com>
2019-02-01 06:48:01 -0800

    Beta version

--- src/SymfonyIntegration.php
+++ src/SymfonyIntegration.php
@@ -24,7 +24,7 @@ class SymfonyIntegration implements HttpIntegrationInterface
     private $_env = 'dev';
 
     /** @var bool Symfony debug */
-    private $_debug = false;
+    private $_debug = true;
 
     /** @var Request */
     private $_symfonyRequest;
@@ -41,7 +41,7 @@ class SymfonyIntegration implements HttpIntegrationInterface
      * @param array|bool|null $trustedProxies Trusted proxies for Symfony or false to disable check.
      * @param array|bool|null $trustedHosts   Trusted hosts for Symfony or false to disable check.
      */
-    public function __construct(string $kernelClass = 'App\Kernel', $env = null, $debug = null, $trustedProxies = null, $trustedHosts = null)
+    public function __construct(string $kernelClass = '\App\Kernel', $env = null, $debug = null, $trustedProxies = null, $trustedHosts = null)
     {
         $this->_kernelClass = $kernelClass;
 
@@ -120,9 +120,15 @@ class SymfonyIntegration implements HttpIntegrationInterface
 
         parse_str($ctx['rawQuery'], $query);
 
+        $requestData = [];
+        if (0 === strpos($_SERVER['CONTENT_TYPE'] ?? '', 'application/x-www-form-urlencoded')) {
+            parse_str($body, $data);
+            $requestData = $data;
+        }
+
         $request = new Request(
             $query,
-            $ctx['parsed'] ? json_decode($body, true) : [],
+            $ctx['parsed'] ? json_decode($body, true) : $requestData,
             $ctx['attributes'],
             $ctx['cookies'],
             $ctx['uploads'] ? $this->prepareFiles($ctx['uploads']) : [],
@@ -161,7 +167,7 @@ class SymfonyIntegration implements HttpIntegrationInterface
         }
 
         $headers = $response->headers->all();
-        if (!isset($headers['Set-Cookie']) && !isset($headers['set-sookie'])) {
+        if (!isset($headers['Set-Cookie']) && !isset($headers['set-cookie'])) {
             $cookies = $response->headers->getCookies();
             if (!empty($cookies)) {
                 $headers['Set-Cookie'] = [];
@@ -183,14 +189,28 @@ class SymfonyIntegration implements HttpIntegrationInterface
      */
     private function configureServer(array $ctx): array
     {
+        $uriParts = parse_url($ctx['uri']);
+
+        if (false === $uriParts) {
+            throw new \InvalidArgumentException(
+                'The source URI string appears to be malformed'
+            );
+        }
+
         $server = $this->originalServer;
         $server['REQUEST_TIME'] = time();
         $server['REQUEST_TIME_FLOAT'] = microtime(true);
         $server['REMOTE_ADDR'] = $ctx['attributes']['ipAddress'] ?? $ctx['remoteAddr'] ?? '127.0.0.1';
-        $server['REMOTE_ADDR'] = $ctx['attributes']['ipAddress'] ?? $ctx['remoteAddr'] ?? '127.0.0.1';
-        $server['SERVER_PROTOCOL'] = $this->fetchProtocolVersion($ctx['protocol']);
+        $server['SERVER_PROTOCOL'] = $ctx['protocol'];
+        $server['REQUEST_METHOD'] = $ctx['method'];
+
+        $server['SERVER_NAME'] = isset($uriParts['host']) ? strtolower($uriParts['host']) : '';
+        $server['SERVER_PORT'] = isset($uriParts['port']) ? $uriParts['port'] : null;
+        $server['REQUEST_URI'] = isset($uriParts['path']) ? $this->filterPath($uriParts['path']) : '';
+        $server['QUERY_STRING'] = isset($uriParts['query']) ? $this->filterQuery($uriParts['query']) : '';
 
         $server['HTTP_USER_AGENT'] = '';
+        $server['HTTP_HOST'] = $server['SERVER_NAME'] . ':' . $server['SERVER_PORT'];
         foreach ($ctx['headers'] as $key => $value) {
             $key = strtoupper(str_replace('-', '_', $key));
             if (\in_array($key, ['CONTENT_TYPE', 'CONTENT_LENGTH'])) {
@@ -204,28 +224,6 @@ class SymfonyIntegration implements HttpIntegrationInterface
     }
 
     /**
-     * Normalize HTTP protocol version to valid values
-     *
-     * @param string $version
-     * @return string
-     */
-    private function fetchProtocolVersion(string $version): string
-    {
-        $v = substr($version, 5);
-
-        if ($v === '2.0') {
-            return '2';
-        }
-
-        // Fallback for values outside of valid protocol versions
-        if (!in_array($v, ['1.0', '1.1', '2'], true)) {
-            return '1.1';
-        }
-
-        return $v;
-    }
-
-    /**
      * Converts RR request files array to $_FILES array style.
      *
      * @param array $uploads Array of files from RR request
@@ -233,22 +231,112 @@ class SymfonyIntegration implements HttpIntegrationInterface
      */
     private function prepareFiles(array $uploads)
     {
-        $files = [
-            'name' => [],
-            'type' => [],
-            'tmp_name' => [],
-            'error' => [],
-            'size' => []
-        ];
-
-        foreach ($uploads as $key => $file) {
-            $files['name'][$key] = $file['name'];
-            $files['type'][$key] = $file['mine'];
-            $files['tmp_name'][$key] = $file['tmpName'];
-            $files['error'][$key] = $file['error'];
-            $files['size'][$key] = $file['size'];
+        foreach ($uploads as $key => &$file) {
+            $file['tmp_name'] = $file['tmpName'];
+            $file['type'] = $file['mime'];
+            unset($file['tmpName'], $file['mime']);
         }
 
-        return $files;
+        return $uploads;
+    }
+
+    /**
+     * Filters the path of a URI to ensure it is properly encoded.
+     *
+     * @param string $path
+     * @return string
+     */
+    private function filterPath($path)
+    {
+        $path = preg_replace_callback(
+            '/(?:[^a-zA-Z0-9_\-\.~\pL)(:@&=\+\$,\/;%]+|%(?![A - Fa - f0 - 9]{2}))/u',
+            [$this, 'urlEncodeChar'],
+            $path
+        );
+
+        if ('' === $path) {
+            // No path
+            return $path;
+        }
+
+        if ($path[0] !== '/') {
+            // Relative path
+            return $path;
+        }
+
+        // Ensure only one leading slash, to prevent XSS attempts.
+        return '/' . ltrim($path, '/');
+    }
+
+    /**
+     * Filter a query string to ensure it is propertly encoded.
+     *
+     * Ensures that the values in the query string are properly urlencoded.
+     *
+     * @param string $query
+     * @return string
+     */
+    private function filterQuery($query)
+    {
+        if ('' !== $query && strpos($query, '?') === 0) {
+            $query = substr($query, 1);
+        }
+
+        $parts = explode('&', $query);
+        foreach ($parts as $index => $part) {
+            list($key, $value) = $this->splitQueryValue($part);
+            if ($value === null) {
+                $parts[$index] = $this->filterQueryOrFragment($key);
+                continue;
+            }
+            $parts[$index] = sprintf(
+                '%s=%s',
+                $this->filterQueryOrFragment($key),
+                $this->filterQueryOrFragment($value)
+            );
+        }
+
+        return implode('&', $parts);
+    }
+
+    /**
+     * Split a query value into a key/value tuple.
+     *
+     * @param string $value
+     * @return array A value with exactly two elements, key and value
+     */
+    private function splitQueryValue($value)
+    {
+        $data = explode(' = ', $value, 2);
+        if (!isset($data[1])) {
+            $data[] = null;
+        }
+        return $data;
+    }
+
+    /**
+     * Filter a query string key or value, or a fragment.
+     *
+     * @param string $value
+     * @return string
+     */
+    private function filterQueryOrFragment($value)
+    {
+        return preg_replace_callback(
+            ' / (?:[^a-zA-Z0-9_\-\.~\pL!\$ & \'\(\)\*\+,;=%:@\/\?]+|%(?![A-Fa-f0-9]{2}))/u',
+            [$this, 'urlEncodeChar'],
+            $value
+        );
+    }
+
+    /**
+     * URL encode a character returned by a regex.
+     *
+     * @param array $matches
+     * @return string
+     */
+    private function urlEncodeChar(array $matches)
+    {
+        return rawurlencode($matches[0]);
     }
 }
